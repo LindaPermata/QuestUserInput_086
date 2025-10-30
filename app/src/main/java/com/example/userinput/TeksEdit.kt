22c7dcd6c1cd7c7e164f6fa9c3ab7ba17b37bb2b
LindaPermata <permatalinda531@gmail.com>
2025-10-30 12:30:49 +0700

    Menambahkan ElevetedCard di form

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -94,7 +98,7 @@ fun FormDataDiri(modifier: Modifier
                 alamat=textAlamat
             }
         ) {
-            Text(stringResource(""))
+            Text(stringResource(R.string.submit))
         }
 
         Divider( modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
@@ -103,6 +107,17 @@ fun FormDataDiri(modifier: Modifier
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+
+        ElevatedCard (
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors= CardDefaults,cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(100.dp)
+                .width(300.dp)
+
+        ){
+
+        }
 
     }
 }
